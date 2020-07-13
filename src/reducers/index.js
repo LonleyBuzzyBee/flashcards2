42abbe16f14004894c896580bf1a2053d8fac6af
mai cook <mailiecook@yahoo.com>
2020-07-13 13:54:47 -0700

    integrate firebase into project

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,15 @@
 import formVisibleReducer from './form-visible-reducer';
 import { combineReducers } from 'redux';
 import cardListReducer from './flash-card-list-reducer';
+import { firestoreReducer } from 'redux-firestore';
 
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
-  masterCardList: cardListReducer
+  masterCardList: cardListReducer,
+  firestore: firestoreReducer
 });
 
 export default rootReducer;
+
+
+
